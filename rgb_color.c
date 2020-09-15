61c13294a95a3c99c532eb5cb81b975846d13b1e
Kozak24 <32338092+Kozak24@users.noreply.github.com>
2020-09-15 11:50:31 +0300

    Updated rgb_color.c

--- rgb_color.c
+++ rgb_color.c
@@ -1,9 +1,10 @@
-#include "rgb_color.h"
+#include "ezcolor.h"
 
 cy_rslt_t init_colors( void )
 {
   return cy_rgb_led_init( CYBSP_LED_RGB_RED, CYBSP_LED_RGB_GREEN, CYBSP_LED_RGB_BLUE, CY_RGB_LED_ACTIVE_LOW );
 }
+
 void color( color_t color )
 {
   cy_rgb_led_on( color, CY_RGB_LED_MAX_BRIGHTNESS );
